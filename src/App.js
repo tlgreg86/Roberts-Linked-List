e805271902299dec7dab1a1be4c668843013060f
Travis <23178810+tlgreg86@users.noreply.github.com>
2018-10-18 21:15:58 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import MainPage from './containers/MainPage/MainPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* 
+          Containers:
+            MainPage.js
+            InputSection.js
+            Cards.js
+          Components:
+            InputField.js
+            Button.js
+            Header.js
+            Card.js
+        */}
+        <MainPage />
       </div>
     );
   }
